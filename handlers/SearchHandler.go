da5cdb9722b5f0a5014fc3e65ce449f5e9d8537e
MrLordCat <141771825+MrLordCat@users.noreply.github.com>
2024-06-08 10:25:46 +0300

    Update SearchHandler.go

--- handlers/SearchHandler.go
+++ handlers/SearchHandler.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"database/sql"
-	"fmt"
 	"literary-lions-forum/handlers/db"
 	"literary-lions-forum/utils"
 	"net/http"
@@ -15,18 +14,55 @@ func SearchHandler(dbConn *sql.DB) http.HandlerFunc {
 			http.Error(w, "Search query is required", http.StatusBadRequest)
 			return
 		}
-		fmt.Println(query)
+
 		results, err := db.SearchAll(dbConn, query)
 		if err != nil {
 			http.Error(w, "Failed to fetch search results", http.StatusInternalServerError)
 			return
 		}
 
-		data := map[string]interface{}{
-			"Results":  results,
-			"Title":    "Search Results",
-			"LoggedIn": true,
+		loggedInUserID, _ := GetUserIDFromSession(r)
+		isAdmin := false
+		if loggedInUserID > 0 {
+			user, err := db.GetUserByID(dbConn, loggedInUserID)
+			if err == nil && user.IsAdmin {
+				isAdmin = true
+			}
 		}
+
+		options := map[string]bool{
+			"notifications": true,
+		}
+		pageData, err := utils.GetPageData(dbConn, loggedInUserID, options)
+		if err != nil {
+			http.Error(w, "Failed to fetch data: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := map[string]interface{}{
+			"Results":             results,
+			"Title":               "Search Results",
+			"LoggedIn":            true,
+			"IsAdmin":             isAdmin,
+			"Notifications":       pageData.Notifications,
+			"UnreadNotifications": pageData.UnreadNotifications,
+		}
+
+		// Подготовка данных для шаблона posts
+		postsData := struct {
+			IsProfile     bool
+			IsAdmin       bool
+			Notifications []db.Notification
+			Posts         []db.Post
+		}{
+			IsProfile:     false,
+			IsAdmin:       isAdmin,
+			Posts:         results.Posts,
+			Notifications: pageData.Notifications,
+		}
+
+		// Включение данных для шаблона posts
+		data["PostsData"] = postsData
 
 		utils.RenderTemplate(w, "searchResults.html", data)
 	}
